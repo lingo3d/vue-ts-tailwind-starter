dbc880963c31eb84f24e5c6c1776fe69c38059ff
lingosandi <91113613+lingosandi@users.noreply.github.com>
2022-04-18 01:30:33 +0800

    code uploaded

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { World, Cube } from "lingo3d-vue"
+</script>
+
+<template>
+  <World>
+    <Cube :animation="{ rotationY: [0, 180, 360] }" />
+  </World>
+</template>
